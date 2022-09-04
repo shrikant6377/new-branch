97bac39366b34852e9332d25b8900f95cd38ffe7
shrikant <kantshri561@gmail.com>
2022-09-04 13:24:51 +0530

    final commit

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// instal cherio
+// instal request
+// instal fs
 
 const cherio = require('cherio');
  const request = require('request');
@@ -28,53 +31,3 @@ request('https://www.growpital.com', (err, resp, html)=>{
     }
 
 });
-
-// const puppeteer = require("puppeteer");
-// const fs = require("fs");
-// const path = require("path");
- 
-// (async () => {
-//   const browser = await puppeteer.launch();
-
-//   const page = await browser.newPage();
-//   page.on("response", async (response) => {
-//     const url = response.url();
-//     if (response.request().resourceType() === "image") {
-//       response.buffer().then((file) => {
-//         const fileName = url.split("/").pop();
-//         const filePath = path.resolve(__dirname, fileName);
-//         const writeStream = fs.createWriteStream(filePath);
-//         writeStream.write(file);
-//       });
-//     }
-//   });
-//   await page.goto("https://freemediatools.com/");
-//   await browser.close();
-// })();
-
-
-
-
-
-
-// const cheerio = require('cheerio');
-// const axios = require('axios');
-
-// const url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250';
-
-// let movies = [];
-// axios.get(url).
-// then((response) => {
-
-//     let $ = cheerio.load(response.data);
-//     $('.lister-list tr').each(function(el , index){
-//         let url = $(this).find('td.titleColumn a').attr('href');
-//         let title = $(this).find('td.titleColumn a').text();
-//         let rating = $(this).find('td.imdbRating').text();
-//         movies.push({ title: title, rating: rating , url : url});
-//     });
-//     console.log(movies);
-
-// }).catch((error)=>{
-//     console.log(error);
-// })
